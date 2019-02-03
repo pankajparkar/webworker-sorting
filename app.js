a3727e500b59546a9aac31cdfdbf74b62ab598da
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-02-04 00:49:37 +0530

    Added psuedo code for interval call.

--- app.js
+++ app.js
@@ -13,13 +13,22 @@ worker.addEventListener('message', function({ data: {type, values}}) {
   }
 }, false);
 
+function createIntervalInstance (fn, timer) {
+  return setInterval(fn, timer)
+}
+
 window.onload = function () {
-  var sortButton = document.getElementById('sort-click')
-  var intervalInput = document.getElementById('interval')
+  var sortButton = document.getElementById('sort-click'),
+    intervalInput = document.getElementById('interval'),
+    intervalCall;
   sortButton.addEventListener('click', function sortClick() {
     worker.postMessage({type: 'SORT'});
     intervalInput.disabled = true
     sortButton.disabled = true
+    intervalCall = createIntervalInstance(() => {
+      // TODO: send value to webworker
+      // get out put back and print it on screen
+    }, 500)
   })
   intervalInput.addEventListener('keyup', function intervalKeyup({target: {value}}) {
     sortButton.disabled = !value
