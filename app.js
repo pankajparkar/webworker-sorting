6c6e9f555f8f1d00560e2762757cd4f1c0758429
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-02-05 03:25:39 +0530

    Its working but not reflecting on UI intermittently.

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-var sharedWebWorker = new Worker('shared-web-worker.js');
+var sharedWebWorker = new Worker('shared-web-worker.js?'+Date.now());
+
+function gernerateRandomNumber(count) {
+  var result = []
+  for (var i = 0; i < count; i++) {
+    // TODO: generate whole number instead
+    result.push(Math.random())
+  }
+  return result
+}
+
+state = {
+  startIndex: 0,
+  collection: gernerateRandomNumber(100000),
+  totalTimeTaken: 0,
+  pause: false
+}
 
 function createIntervalInstance (fn, timer) {
   return setInterval(fn, timer)
@@ -7,26 +23,26 @@ function createIntervalInstance (fn, timer) {
 window.onload = function () {
   var sortButton = document.getElementById('sort-click'),
     intervalInput = document.getElementById('interval'),
+    status = document.getElementById('status'),
     intervalCall;
-debugger
   sortButton.addEventListener('click', function sortClick() {
-    worker.postMessage({trigger: 'SORT'});
-    intervalInput.disabled = true
-    sortButton.disabled = true
+    sharedWebWorker.postMessage({trigger: 'SET_COLLECTION', state: state})
     intervalCall = createIntervalInstance(() => {
       // TODO: send value to webworker
       var randomNumber = Math.random()
-      worker.pause = true
-      worker.postMessage({trigger: 'INTERVAL', values: randomNumber})
-      // worker.terminate();
-    }, 500)
+      sharedWebWorker.postMessage({trigger: 'INTERVAL', value: randomNumber});
+    }, intervalInput.value)
+    // setTimeout(() => sharedWebWorker.postMessage({trigger: 'SORTING', state: state}))
+    sharedWebWorker.postMessage({trigger: 'SORTING', state: state})
+    intervalInput.disabled = true
+    sortButton.disabled = true
   })
 
   intervalInput.addEventListener('keyup', function intervalKeyup({target: {value}}) {
     sortButton.disabled = !value
   })
 
-  worker.addEventListener('message', function({ data: {trigger, values}}) {
+  sharedWebWorker.addEventListener('message', function({ data: {trigger, state}}) {
     switch (trigger) {
       case 'SORTED':
         var sortButton = document.getElementById('sort-click')
@@ -36,7 +52,11 @@ debugger
         clearInterval(intervalCall)
         intervalCall = null
         break;
-      default:
+      case 'UI':
+        console.log('state on ui', state)
+        var div = document.createElement('div')
+        div.innerHTML = 'Till now only '+ state.startIndex + ' element has been started, and time taken '+ state.totalTimeTaken + 'ms'
+        status.appendChild(div)
         break;
     }
   }, false);
--- shared-web-worker.js
+++ shared-web-worker.js
@@ -1,48 +1,53 @@
-var terminate = self.terminate,
-  collection,
-  totalTimeTaken = 0,
-  startIndex = 0;
-self.test = false
-
-function insertionSort(collection, startIndex = 0) {
-  for (var i = startIndex; i < collection.length; i++) {
-    let currentValue = collection[i]
+self._state = {}
+function insertionSort (state) {
+  var collection = state.collection,
+    startIndex = state.startIndex
+  outerloop: for (var i = startIndex; i < collection.length; i++) {
+    var currentValue = collection[i]
     for (var j = i - 1; j > -1 && collection[j] > currentValue; j--) {
       collection[j + 1] = collection[j]
     }
     collection[j + 1] = currentValue
-    if (self.pause) {
-      startIndex = ++i
-      console.log('Paused', i)
-      break
+    if (self._state.pause) {
+      break outerloop
     }
   }
-  console.log(i)
-  return startIndex
+  self._state.startIndex = ++i
 }
 
-function gernerateRandomNumber(count) {
-  var result = []
-  for (var i = 0; i < count; i++) {
-    // TODO: generate whole number instead
-    result.push(Math.random())
-  }
-  return result
+function setState (state) {
+  self._state = state
 }
 
-self.addEventListener('message', function ({ data: { trigger } }) {
+function applySort(state) {
+  var t0 = performance.now();
+  insertionSort(state)
+  var t1 = performance.now()
+  state.totalTimeTaken += t1 - t0
+  // setTimeout(() => self.postMessage({trigger: 'UI', state: state}))
+  self.postMessage({trigger: 'UI', state: state})
+}
+
+self.addEventListener('message', function ({ data: {state, trigger, value} }) {
   switch (trigger) {
-    case 'SORT':
-      var t0 = performance.now();
-      collection = gernerateRandomNumber(100000)
-      insertionSort(collection, startIndex)
-      var t1 = performance.now()
-      totalTimeTaken += t1 - t0
-      self.postMessage({ trigger: 'SORTED', values: result })
+    case 'SET_COLLECTION':
+      setState(state)
+      break
+    case 'SORTING':
+      // setTimeout(function () {
+        applySort(self._state)
+        while (state.collection.length >= self._state.startIndex) {
+          self._state.pause = false
+          applySort(self._state)
+        }
+        self.postMessage({trigger: 'SORTED'})
+        console.log('Completed', state)
+      // })
       break
     case 'INTERVAL':
-      console.log('inreval fired', startIndex)
-      self.postMessage({ trigger: 'INTERVAL', values: [] })
+      self._state.pause = true
+      console.log(self._state)
+      if(self._state.collection) self._state.collection.push(value)
       break
     default:
       break;
