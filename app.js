483b018821cf089480bbb0de900b3b05ccd282fa
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-02-08 02:34:00 +0530

    Updates

---
+++ app.js
@@ -0,0 +1,83 @@
+var sharedWebWorker = new Worker('web-worker.js?'+Date.now());
+
+var intervalCall, sortedCollection;
+ 
+function gernerateRandomNumber(count) {
+  var result = []
+  for (var i = 0; i < count; i++) {
+    result.push(Math.random())
+  }
+  return result
+}
+
+state = {
+  startIndex: 0,
+  endIndex: 1000,
+  collection: gernerateRandomNumber(100000),
+  totalTimeTaken: 0
+}
+
+function createIntervalInstance (timer) {
+  return setInterval(() => {
+    var randomNumber = Math.random()
+    sharedWebWorker.postMessage({trigger: 'INTERVAL', value: randomNumber});
+  }, timer)
+}
+
+window.onload = function () {
+  var sortButton = document.getElementById('sort-click'),
+    intervalInput = document.getElementById('interval'),
+    stopTimerButton = document.getElementById('stop-timer'),
+    status = document.getElementById('status')
+  sortButton.addEventListener('click', function sortClick() {
+    sharedWebWorker.postMessage({trigger: 'SET_STATE', state: state})
+    sharedWebWorker.postMessage({trigger: 'SORT', state: state})
+    intervalCall = createIntervalInstance(intervalInput.value)
+    stopTimerButton.disabled = false
+    intervalInput.disabled = true
+    sortButton.disabled = true
+  })
+
+  stopTimerButton.addEventListener('click', function () {
+    if (intervalCall){
+      clearInterval(intervalCall)
+      intervalCall = null
+      stopTimerButton.disabled = true
+      state.pause = false
+    }
+  })
+
+  intervalInput.addEventListener('keyup', function intervalKeyup({target: {value}}) {
+    sortButton.disabled = !value
+  })
+
+  function addLog (message, className) {
+    var li = document.createElement('li')
+    li.className = (className || 'list-group-item') + ' ' + 'highlight'
+    li.innerHTML = message
+    status.appendChild(li)
+    status.scrollTop = status.scrollHeight;
+  }
+
+  sharedWebWorker.addEventListener('message', function({ data: {trigger, message, state}}) {
+    switch (trigger) {
+      case 'SORTED':
+        var sortButton = document.getElementById('sort-click')
+        var intervalInput = document.getElementById('interval')
+        intervalInput.disabled = false
+        sortButton.disabled = false
+        if (intervalCall){
+          clearInterval(intervalCall)
+          intervalCall = null
+          stopTimerButton.disabled = true
+        }
+        window.sortedCollection = state.collection
+        addLog(message, 'alert alert-success')
+        break;
+      case 'LOGS':
+        addLog(message)
+        break;
+    }
+  }, false);
+}
+
---
+++ web-worker.js
@@ -0,0 +1,71 @@
+self._state = {}
+function insertionSort(state) {
+  var collection = state.collection,
+    startIndex = state.startIndex,
+    endIndex = state.endIndex
+  outerloop: for (var i = startIndex; i < endIndex; i++) {
+    var currentValue = collection[i]
+    for (var j = i - 1; j > -1 && collection[j] > currentValue; j--) {
+      collection[j + 1] = collection[j]
+    }
+    collection[j + 1] = currentValue
+    if (state.pause) {
+      break outerloop
+    }
+  }
+  state.startIndex = i++
+}
+
+function setState(state) {
+  self._state = state
+}
+
+function applySort(state) {
+  var t0 = performance.now();
+  insertionSort(state)
+  var t1 = performance.now()
+  self._state.totalTimeTaken += t1 - t0
+}
+
+function calculateStartAndEndIndex(state) {
+  state.startIndex = state.endIndex
+  var futureEndIndex = state.endIndex + 1000
+  state.endIndex = futureEndIndex <= state.collection.length ? futureEndIndex : state.collection.length
+  return state
+}
+
+function sorting(state) {
+  var sort = setInterval(function () {
+    if (state.collection.length > state.startIndex + 1) {
+      state.pause = false
+      if (! state.pause) {
+        applySort(state)
+        calculateStartAndEndIndex(state)
+      } else {
+        clearInterval(sort)
+        sorting(state)
+      }
+    } else {
+      clearInterval(sort)
+      self.postMessage({ trigger: 'SORTED', message: `Time taken to complete sorting is ${state.totalTimeTaken} ms`, state })
+    }
+  }, 5)
+}
+
+self.addEventListener('message', function ({ data: { state, trigger, value } }) {
+  switch (trigger) {
+    case 'SET_STATE':
+      setState(state)
+      break
+    case 'SORT':
+      sorting(self._state)
+      break
+    case 'INTERVAL':
+      var t0 = performance.now();
+      self._state.pause = true
+      self._state.collection.push(value)
+      var t1 = performance.now()
+      self.postMessage({ trigger: 'LOGS', message: `Time taken to process message ${(t1 - t0)} ms` })
+      break
+  }
+}, false);
